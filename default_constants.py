1206ca9a98cd85a63504517cad50d0452fb45cd8
truejulosdu13 <julosdu13@yahoo.fr>
2021-02-17 13:37:15 +0100

    Change default constants as a dict

--- default_constants.py
+++ default_constants.py
@@ -64,5 +64,65 @@ def set_default_constants():
     cst_all  = (cst_phys, cst_conc, cst_syst, cst_simu, cst_expe)  
     return(cst_all)
 
+def default_constants():
+    d_cst = {
+        ## PHYSICAL CONSTANTS 
+            "R"       : 8.3145,  # J/mol-K, ideal gas constant
+            "T"       : 298.15,  # K, temperature
+            "F"       : 96485,   # C/mol, Faraday's constant
+            "S"       : 1E-6,    #  electrode surface. Default = 1E-6
+            "F_norm"  : 96485/(8.3145*298.15),
 
+
+        ## INDEPENDENT VARIABLES 
+        ### EXPERIMENTALLY CONTROLED
+            "C_a"     : 2E-3,   #  mol/L, initial concentration of A. Default = 1.0
+            "C_b"     : 0.0,    #  mol/L, initial concentration of B. Default = 0.0
+            "C_c"     : 0.0,    #  mol/L, initial concentration of C. Default = 0.0
+            "C_d"     : 0.0,    #  mol/L, initial concentration of D. Default = 0.0
+            "n_sp"    : 4,      # number of reactive species considered
+
+        ### SYSTEM DEPENDENT
+            "E_0_1"        : -0.5,    #  electrochemical potential couple A/B. Default = 0.0
+            "E_0_2"        : -1.0,    #  electrochemical potential couple C/D. Default = -1.0
+            "D"            : 1E-5,    #  cm^2/s, O & R diffusion coefficient. Default = 1E-5
+            "n"            : 1.0,     #  number of electrons transfered. Default = 1
+            "alpha"        : 0.5,     #  dimensionless charge-transfer coefficient. Default = 0.5
+            "k0"           : 1E-2,    #  cm/s, electrochemical rate constant. Default = 1E-2
+            "kc"           : 1E-3,    #  1/s, chemical rate constant. Default = 1E-3
+            "k_p"          : 1,       #  1/s, chemical rate constant. Default = 1E-3
+            "k_m"          : 1,       #  1/s, chemical rate constant. Default = 1E-3
+            "K"            : 0,     #  dimensionless equilibrium constant for the pure chemical equilibria
+
+        ## SIMULATION VARIABLES
+        # simulation accuracy in time and space
+            "Nt"           : 2000,      # number of iterations per t_k (pg 790). Default = 500
+            "Nx"           : 200,       # number of spatial boxes  Default = 200
+
+        # key adimensionnated parameters
+            "DM"           : 0.45,       #DM = D*Dt/Dx**2 numerical adimensionated diffusion parameter. Default = 0.45
+            "Lambda"       : 50,         #Lambda  = k0*Dx/D numerical adimensionated electron transfer parameter. Fast if 
+
+        # physical distances according to the previosu parameters
+            "L_cuve"       : 0,     # longueur physique de la cuve so that we are always in a diffusion controled system
+            "Dx"           : 0,     # pas d'espace
+
+        ## Experimental paramters for all voltammetry techniques
+        ## LSV
+            "E_i"          : +0.0,
+            "E_ox"         : +0.5,
+            "E_red"        : -1.5,
+            "E_SW"         : +0.05,
+            "Delta_E"      : -0.01,
+            "v"            : 0.1,
+            "f"            : 25,
+            "tau"          : 1.0
+            }
+    L_cuve  = math.floor((d_cst["D"]*d_cst["Nt"])**0.5) + 2
+    Dx      = L_cuve/d_cst["Nx"]                      # pas d'espace
+    K       = d_cst["k_p"]/d_cst["k_m"]
+    d_cst["K"]  = K
+    d_cst["Dx"] = Dx
+    d_cst["L_cuve"] = L_cuve
+    return(d_cst)
  
